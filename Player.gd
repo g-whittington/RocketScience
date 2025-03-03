7ca1d01024523d41a57d061aee99db3bb334f949
George Whittington <lwhittington2231@gmail.com>
2025-03-02 22:48:05 -0500

    Created Input Map and added to Player

--- Player.gd
+++ Player.gd
@@ -3,11 +3,11 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("fly"):
 		position.y += delta
 
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("rotate_left"):
 		rotate_z(delta)
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("rotate_right"):
 		rotate_z(-delta)
