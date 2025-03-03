804011245edcf2e00f944c3a6ffd3eb014d4c767
George Whittington <lwhittington2231@gmail.com>
2025-03-03 00:41:51 -0500

    Updated Input Map Changed flt to boost

--- Player.gd
+++ Player.gd
@@ -5,7 +5,7 @@ var rotation_force := 100.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("fly"):
+	if Input.is_action_pressed("boost"):
 		# basis here is a local value of the y axis that will change 
 		# the shape's actally y, and not just apply a general up force
 		apply_central_force(basis.y * delta * flight_force)
